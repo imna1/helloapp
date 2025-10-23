64b7e13de4c185dbc6a5f0635bcea76b8e1b5010
black <black@example.com>
2025-10-23 17:42:18 +0300

    Add GreatBlack

--- HelloAppBlack/HelloAppBlack/Program.cs
+++ HelloAppBlack/HelloAppBlack/Program.cs
@@ -13,5 +13,10 @@ namespace HelloAppBlack
         {
             Console.WriteLine("hello white")
         }
+
+        static void GreetBlack()
+        {
+            Console.WriteLine("hello black")
+        }
     }
 }
